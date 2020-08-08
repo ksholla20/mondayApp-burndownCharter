e23d91bf0a81f6426dbf0b4a9d3195dc518e656a
ksholla20 <ksholla20@gmail.com>
2020-08-08 20:01:15 +0530

    Working code of Burndown Charter

---
+++ README.md
@@ -0,0 +1,38 @@
+## Overview
+This is a Monday app to display burndown chart for a given group.
+There is a dropdown, where you can choose the group for which burndown chart is to be displayed
+By default, top group is selected
+
+## Run the project
+
+In the project directory, you should run:
+
+### `npm install`
+
+And then to run an application with automatic virtual ngrok tunnel, run:
+
+### `npm start`
+
+Visit http://localhost:4040/status and under "command_line section" find the URL. This is the public URL of your app, so you can use it to test it.
+F.e.: https://021eb6330099.ngrok.io
+
+## Configure Monday App 
+
+1. Open monday.com, login to your account and go to a "Developers" section.
+2. Create a new "QuickStart View Example App"
+3. Open "OAuth & Permissions" section and add "boards:read" and "updates:read" scope
+4. Open "Features" section and create a new "Boards View" feature
+5. Open "View setup" tab and fulfill in "Custom URL" field your ngrok public URL, which you got previously (f.e. https://021eb6330099.ngrok.io)
+6. Click "Boards" button and choose one of the boards with some data in it.
+7. Click "Preview button"
+8. Enjoy the Burndown chart app!
+
+## Release your app
+1. Run script
+### `npm run build`
+2. Zip your "./build" folder
+3. Open "Build" tab in your Feature
+4. Click "New Build" button
+5. Click "Upload" radio button and upload zip file with your build
+6. Go to any board and add your just released view
+7. Enjoy!
---
+++ src/App.js
@@ -0,0 +1,134 @@
+import React from "react";
+import "./App.css";
+import mondaySdk from "monday-sdk-js";
+import { Dropdown } from 'office-ui-fabric-react/lib/Dropdown';
+import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
+import { CommandBarButton } from 'office-ui-fabric-react';
+import { exportComponentAsJPEG, exportComponentAsPDF, exportComponentAsPNG } from "react-component-export-image";
+import {LineGraph} from "./chartUtils";
+const monday = mondaySdk();
+
+const dropdownStyles = {
+  dropdown: { width: 300 },
+};
+
+const buttonStyles = {
+    root: {height: 44, marginTop: 20}
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.componentRef = React.createRef();
+
+    // Default state
+    this.state = {
+      settings: {},
+      name: "",
+    };
+  }
+
+    updateChartData(groupId, groupTitle) {
+      const items = this.state.boardData.boards[0].items.filter((v=>(v.group.id === groupId))).map((val)=>({
+          "id": val.id,
+          "updates": val.updates,
+          "status": val.column_values.filter((cval)=>(cval.title === "Status"))[0].text,
+          "estimate": Number(val.column_values.filter((cval)=>(cval.title === "Time Est."))[0].text),
+      }));
+      const tot = items.reduce((acc,val)=>acc+val.estimate,0);
+      const dn = items
+      .filter((val)=>(val.status === "Done"))
+      .map((val)=>({...val, "last_updated": val.updates.filter((vl=>(vl.text_body.split("").reverse().join("").indexOf("enoD") === 0)))[0].updated_at}))
+      .reduce(function(acc,val){const d = new Date(val.last_updated); const str = [d.getFullYear(),d.getMonth(),d.getDate()].join("/");acc[str] = ((str in acc)?acc[str]:0)+val.estimate; return acc},{});
+      const cumulativeSum = (sum => value => sum -= value)(tot);
+      const burnStatus = Object.keys(dn).map((k)=>({"date":k,"val":dn[k]})).sort((a,b)=>(new Date(a.date)).getTime() - (new Date(b.date)).getTime()).map((v)=>({...v,"cur":cumulativeSum(v.val)}));
+      const lineData = {
+          "labels": ["Begin", ...burnStatus.map((v)=>(v.date))],
+          "datasets":[{
+              "label": groupTitle,
+              "data": [tot, ...burnStatus.map((v)=>(v.cur))]
+          }]
+      };
+      this.setState({lineData:lineData, selectedGroupId:groupId, selectedGroupTitle:groupTitle});
+  }
+
+  componentDidMount() {
+      initializeIcons();
+      monday.listen("settings", res => {
+      this.setState({ settings: res.data });
+    });
+      
+    monday.listen("context", res => {
+      this.setState({context: res.data});
+      monday.api(`query ($boardIds: [Int]) { boards (ids:$boardIds) { name items {name id group { id } updates {text_body updated_at} column_values { title text } } top_group { id title }  groups { id title } } }`,
+        { variables: {boardIds: this.state.context.boardIds} }
+      )
+      .then(res => {
+        this.setState({boardData: res.data});
+        this.updateChartData(res.data.boards[0].top_group.id, res.data.boards[0].top_group.title);
+      });
+    })
+
+  }
+  onGroupSelect = (ev, val)=>{
+    this.updateChartData(val.key, val.text);
+  }
+
+  render() {
+    const menuProps = {
+      items: [
+        {
+          key: 'downloadPDF',
+          text: 'Download PDF',
+          onClick: () => exportComponentAsPDF(this.componentRef, `${this.state.selectedGroupTitle}.pdf`),
+        },
+        {
+          key: 'downloadJPEG',
+          text: 'Download JPEG',
+          onClick: () => exportComponentAsJPEG(this.componentRef, `${this.state.selectedGroupTitle}.jpeg`),
+        },
+        {
+          key: 'downloadPNG',
+          text: 'Download PNG',
+          onClick: () => exportComponentAsPNG(this.componentRef, `${this.state.selectedGroupTitle}.png`),
+        }
+      ],
+    };
+    let options = undefined;
+    if (this.state.boardData) {
+        options = this.state.boardData.boards[0].groups.map((v)=>({"key":v.id,"text":v.title}));
+    }
+
+    return (
+      <div
+        className="App"
+      >
+        { options && 
+            <Dropdown
+              placeholder="Select an option"
+              label="Group for which burndown chart is to be displayed"
+              defaultSelectedKey={this.state.selectedGroupId}
+              options={options}
+              styles={dropdownStyles}
+              onChange={this.onGroupSelect}
+            />
+        }
+        <div style={{background: (this.state.settings.background || "white")}} ref={this.componentRef}>
+          {this.state.lineData && <LineGraph data={this.state.lineData}/>}
+        </div>
+        {this.state.lineData && 
+          <CommandBarButton
+            iconProps={{ iconName: 'Download' }}
+            text="Download this chart"
+            checked={true}
+            menuProps={menuProps}
+            styles={buttonStyles}
+          />
+        }
+      </div>
+    );
+  }
+
+}
+
+export default App;
